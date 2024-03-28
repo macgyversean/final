d4510dc6602372dd00d6da237799a23a3ce7d6e4
Sean Carroll <macgyversean@gmail.com>
2024-03-28 13:03:45 -0400

    updated origins

--- fastapi-supabase/main.py
+++ fastapi-supabase/main.py
@@ -28,7 +28,8 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://seanexperience.netlify.app"
+    "https://seanexperience.netlify.app",
+    "https://seanexperience.netlify.app/login"
     
 ]
 
