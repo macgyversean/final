567da89aee22e4ee84455c1a248f0615254d9963
Sean Carroll <macgyversean@gmail.com>
2024-04-04 10:57:04 -0400

    added in a chatbot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import Routes from "./routes/Routes";
 import { AuthProvider } from "./AuthContext";
+import { Crisp } from "crisp-sdk-web";
+import { Component } from "react";
 
-function App() {
-  return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
-  );
+class App extends Component {
+  componentDidMount() {
+    Crisp.configure("7fa207c6-a4a7-4b2d-a001-a14f2088f8f9");
+  }
+
+  render() {
+    return (
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    );
+  }
 }
-
 export default App;
